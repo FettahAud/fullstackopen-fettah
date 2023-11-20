600f049830f36f36ae01b60933b9b5e0098004b5
fettahaud <fettahaud@gmail.com>
2023-11-20 14:51:16 +0300

    ex 1.11

--- part1/d/unicafe/src/App.jsx
+++ part1/d/unicafe/src/App.jsx
@@ -4,24 +4,46 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
 const Statistics = (props) => {
   if (props.good + props.neutral + props.bad === 0) {
     return <p>No Feedback given</p>;
   }
   return (
     <div>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.good + props.neutral + props.bad}</p>
-      <p>
-        Average:
-        {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
-      </p>
-      <p>
-        Positive:
-        {(props.good / (props.good + props.neutral + props.bad)) * 100}%
-      </p>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={props.good} />
+          <StatisticLine text="Neutral" value={props.neutral} />
+          <StatisticLine text="Bad" value={props.bad} />
+          <StatisticLine
+            text="All"
+            value={props.good + props.neutral + props.bad}
+          />
+          <StatisticLine
+            text="Average"
+            value={
+              (props.good - props.bad) /
+              (props.good + props.neutral + props.bad)
+            }
+          />
+          <StatisticLine
+            text="Positive"
+            value={`${
+              (props.good / (props.good + props.neutral + props.bad)) * 100
+            }%`}
+          />
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -38,20 +60,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
-      {/* <Statistic text="Good:" value={good} />
-      <Statistic text="Neutral:" value={neutral} />
-      <Statistic text="Bad:" value={bad} />
-      <Statistic text="All:" value={good + neutral + bad} />
-      <Statistic
-        text="Average:"
-        value={(good - bad) / (good + neutral + bad)}
-      />
-      <Statistic
-        text="Positive:"
-        value={`${(good / (good + neutral + bad)) * 100}%`}
-      /> */}
     </div>
   );
 };
